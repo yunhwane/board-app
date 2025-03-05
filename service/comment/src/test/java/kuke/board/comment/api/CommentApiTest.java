93e09547f78fa71e55649da4c6c02889f42e89aa
yunhwane <wjsdbsghks96@dgu.ac.kr>
2025-03-05 16:00:11 +0900

    feat: 댓글 API 테스트 및 데이터 1200만 건 초기화

---
+++ service/comment/src/test/java/kuke/board/comment/api/CommentApiTest.java
@@ -0,0 +1,62 @@
+package kuke.board.comment.api;
+
+import kuke.board.comment.service.response.CommentResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class CommentApiTest {
+
+    RestClient restClient = RestClient.create("http://localhost:9002");
+
+
+    @Test
+    void create() {
+        CommentResponse response1 = createComment(new CommentCreateRequest(1L, "my comment1", null, 1L));
+        CommentResponse response2 = createComment(new CommentCreateRequest(1L, "my comment2", response1.getCommentId(), 1L));
+        CommentResponse response3 = createComment(new CommentCreateRequest(1L, "my comment3", response1.getCommentId(), 1L));
+
+        System.out.println("commentId=%s".formatted(response1.getCommentId()));
+        System.out.println("\tcommentId=%s".formatted(response2.getCommentId()));
+        System.out.println("\tcommentId=%s".formatted(response3.getCommentId()));
+    }
+
+
+    @Test
+    void read() {
+        CommentResponse response = restClient.get()
+                .uri("/v1/comments/{commentId}", 155566697792507904L)
+                .retrieve()
+                .body(CommentResponse.class);
+
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void delete() {
+        restClient.delete()
+                .uri("/v1/comments/{commentId}", 155566697926725632L)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    CommentResponse createComment(CommentCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/comments")
+                .body(request)
+                .retrieve()
+                .body(CommentResponse.class);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CommentCreateRequest {
+        private Long articleId;
+        private String content;
+        private Long parentCommentId;
+        private Long writerId;
+    }
+
+
+}
---
+++ service/comment/src/test/java/kuke/board/comment/data/DataInitializer.java
@@ -0,0 +1,71 @@
+package kuke.board.comment.data;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import kuke.board.comment.entity.Comment;
+import kuke.board.common.snowflake.Snowflake;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 데이터 1200만건 벌크 연산
+ *
+ * 데이터 초기화시 쓰레드 10개로 벌크 연산
+ */
+
+@SpringBootTest
+public class DataInitializer {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    TransactionTemplate transactionTemplate;
+    Snowflake snowflake = new Snowflake();
+    CountDownLatch latch = new CountDownLatch(EXECUTE_COUNT);
+
+    static final int BULK_INSERT_SIZE = 2000;
+    static final int EXECUTE_COUNT = 6000;
+
+    @Test
+    void initialize() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < EXECUTE_COUNT; i++) {
+            executorService.submit(() -> {
+                insert();
+                latch.countDown();
+                System.out.println("latch count: " + latch.getCount());
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+    }
+
+    void insert() {
+        transactionTemplate.executeWithoutResult(status -> {
+            Comment prev = null;
+            for (int i = 0; i < BULK_INSERT_SIZE; i++) {
+
+                Comment comment = Comment.create(
+                        snowflake.nextId(),
+                        "content",
+                        i % 2 == 0 ? null : prev.getCommentId(),
+                        1L,
+                        1L
+                );
+
+                prev = comment;
+
+                em.persist(comment);
+            }
+        });
+    }
+}
